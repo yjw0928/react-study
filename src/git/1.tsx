4dedf28560627c5147c3ec05e6a01a42c7adf400
杨吉文 <943421434@qq.com>
2023-02-02 12:24:00 +0800

    feat:git 测试一下

--- src/git/1.tsx
+++ src/git/1.tsx
@@ -4,7 +4,13 @@ const Demo = () => {
   return (
     <div>
       这是开始的git
-      <code lang="git">1111</code>
+      <code lang="git">
+        <span>git reset</span>
+        <span>hard: 直接回退本地代码到指定的版本</span>
+        <span>soft: 回退本地代码到指定版本，同事将后面版本的改动暂存</span>
+      </code>
     </div>
   );
 };
+
+export default Demo;
