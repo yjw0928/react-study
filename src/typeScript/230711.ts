9038b295103a0c954004a21b57da7f6d12a301cd
杨吉文 <943421434@qq.com>
2023-07-12 00:00:15 +0800

    feat:ts接口继承类

---
+++ src/typeScript/230711.ts
@@ -0,0 +1,31 @@
+class A {
+  a: number;
+  private c: string;
+  static d: number;
+  private static f: number;
+  protected g: number;
+  constructor() {
+    this.a = 1;
+  }
+}
+
+class BCls extends A {
+  constructor() {
+    super();
+    this.g = 1;
+  }
+}
+
+console.log(BCls.d);
+
+// 接口继承类
+interface B extends A {
+  e: boolean;
+}
+
+const bDict: B = {
+  a: 1,
+  e: true,
+  g: 1,
+  c: "ddd",
+};
